4744d0074a96341a10580fb513f5d957615761fa
Arctorus0422 <99332803+HanHua0422@users.noreply.github.com>
2022-04-15 12:36:11 +0100

    Update IR.py

--- Sensors/IR.py
+++ Sensors/IR.py
@@ -1,1 +1,18 @@
+import RPi.GPIO as GPIO
 import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(26,GPIO.IN)
+
+ir_sensor =  GPIO.input(26)
+
+def main():
+  while True:
+  
+    if sensor ==  1:
+      print("Line detected")
+    
+      else:
+        print("Line not detected")
+      
