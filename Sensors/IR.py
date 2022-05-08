05b66e69b63b989d1d87e5a17a46a031a727f3d7
Arctorus0422 <99332803+HanHua0422@users.noreply.github.com>
2022-05-08 16:12:59 +0100

    Update IR.py

--- Sensors/IR.py
+++ Sensors/IR.py
@@ -1,4 +1,4 @@
-from gpiozero import Robot, LineSensor
+from gpiozero import Robot, LineSensor, DistanceSensor
 from signal import pause
 from time import sleep
 
@@ -6,28 +6,35 @@ from time import sleep
 # Assign pins to left and right motors
 robot = Robot(left=(7, 8), right=(9, 10))
 
-# Assign pins to infrared sensors
+# Assign pins to infrared sensors and ultrasonic sensor
 left_sensor = LineSensor(17)
 right_sensor= LineSensor(27)
+# robot should move backwards if obstacle gets within 10cm
+ultra = DistanceSensor(23, 24, max_distance=1, threshold_distance=0.1)
 
 # Constant loop
 while True:
   left_detect = int(left_sensor.value)
   right_detect = int(right_sensor.value)
+  ultra_detect = ultra.value
+  ultra_sensor = format(round(ultra_detect,1)))
   
 # Possible scenarios
 # 1: left detects, right doesn't detect
-  if left_sensor.when_line and right_sensor.when_no_line:
+  if left_detect == 1 and right_detect == 0:
     robot.turn_left()
 
 # 2: right detects, left doesn't detect
-  elif left_sensor.when_no_line and right_sensor.when_line:
+  elif left_detect == 0 and right_detect == 1:
     robot.turn_right()
 
 # 3: left and right detect
-  elif left_sensor.when_line and right_sensor.when_line:
+  elif left_detect == 1 and right_detect == 1:
     robot.move_forward()
   
 # 4: left and right don't detect
-  elif left_sensor.when_no_line and left_sensor.when_no_line:
+  elif left_detect == 0 and right_detect == 0:
     robot.stop()
+  
+  elif ultra_sensor <= 0.1:
+    robot.move_backwards()
